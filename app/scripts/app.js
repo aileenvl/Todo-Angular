6cd3f84058ac642fe32ad83a2d96a476804ce698
Aileen <aileenvl@gmail.com>
2014-12-21 16:28:20 -0600

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name todoApp
+ * @description
+ * # todoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('todoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ui.sortable'
+  ]);
