25828f4292a160e113034df42551f7d89ac38906
Aileen <aileenvl@gmail.com>
2014-12-21 16:30:22 -0600

    second commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('todoApp')
    .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.todos = ['buy food', 'Item 2', 'Item 3'];
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
